1465384a9228efdf1cebfbd972a94873864e63de
Cahuete0512 <magalie.contant@epsi.fr>
2023-02-18 17:18:14 +0100

    first commit and add readme file

---
+++ src/main/java/com/b2fsgr2/tp_java_contant_magalie/TpJavaContantMagalieApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_java_contant_magalie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaContantMagalieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpJavaContantMagalieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b2fsgr2/tp_java_contant_magalie/TpJavaContantMagalieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_java_contant_magalie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaContantMagalieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
